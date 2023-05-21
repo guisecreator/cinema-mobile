efdc2b32a7644842733dce9f15526908c531716a
Nikita Semyachkin <93248541+guisecreator@users.noreply.github.com>
2023-05-21 21:35:05 +0500

    added klaus image

--- app/src/main/java/com/example/cinema_mobile/MainActivity.kt
+++ app/src/main/java/com/example/cinema_mobile/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.cinema_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.os.CountDownTimer
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var timer = object : CountDownTimer(8000, 1000)
+        {
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                val intent = Intent(this@MainActivity, PatchActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        timer.start()
     }
 }
---
+++ app/src/main/java/com/example/cinema_mobile/PatchActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cinema_mobile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class PatchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patch)
+    }
+
+}
