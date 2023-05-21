124ad2e519c9d2fd0b873bf7660111b8d1d4b5c8
Nikita Semyachkin <93248541+guisecreator@users.noreply.github.com>
2023-05-21 22:27:00 +0500

    added patchactivity and more

--- app/src/main/java/com/example/cinema_mobile/PatchActivity.kt
+++ app/src/main/java/com/example/cinema_mobile/PatchActivity.kt
@@ -4,11 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.content.SharedPreferences
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class PatchActivity : AppCompatActivity() {
+    lateinit var preff: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patch)
+
+        preff = getSharedPreferences("TABLEE", MODE_PRIVATE)
+
+        val poster_res:RecyclerView=findViewById((R.id.recview1))
+        poster_res.adapter = PosterAdapter(this, PosterList().list)
+        val actor_res:RecyclerView=findViewById((R.id.recview2))
+        actor_res.adapter = ActorAdapter(this, ActorList().list2)
     }
 
 }
